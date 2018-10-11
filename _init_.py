eda0950310242d5696e6bc6ae6c9dd816f711658
aemon4 <43272212+aemon4@users.noreply.github.com>
2018-10-11 17:06:19 +0200

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 15:45:07 2018
+
+@author: PC
+"""
+
---
+++ filters.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 15:43:42 2018
+
+@author: PC
+"""
+
